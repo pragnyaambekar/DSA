a198f5335290d7c8159b2d0708368fc20d089f69
Pragnya Ambekar <64370626+pragnyaambekar@users.noreply.github.com>
2025-11-11 20:16:00 -0700

    Add reverse integer function in ReverseInteger.py
    Implement the reverse function to reverse digits of a signed 32-bit integer.

---
+++ ReverseInteger.py
@@ -0,0 +1,29 @@
+'''Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21'''
+
+class Solution(object):
+    def reverse(self, x):
+        num = x
+        rev = 0
+        while num>0:
+            d = num%10
+            rev = rev*10+d
+            num = num//10
+        return rev
+        
